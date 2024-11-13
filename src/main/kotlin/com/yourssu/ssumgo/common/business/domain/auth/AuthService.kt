1717fbf47ebe21dacb3215309c602f4cb6516bef
dwl21 <nggus5@gmail.com>
2024-11-13 22:51:42 +0900

    feat: implement an AuthService

---
+++ src/main/kotlin/com/yourssu/ssumgo/common/business/domain/auth/AuthService.kt
@@ -0,0 +1,44 @@
+package com.yourssu.ssumgo.common.business.domain.auth
+
+import com.yourssu.ssumgo.common.application.domain.auth.RefreshTokenRequest
+import com.yourssu.ssumgo.common.implement.domain.auth.AccessTokenGenerator
+import com.yourssu.ssumgo.common.implement.support.soomsil.*
+import com.yourssu.ssumgo.student.implement.domain.student.StudentWriter
+import org.springframework.stereotype.Service
+
+@Service
+class AuthService(
+    private val authClient: AuthClient,
+    private val userClient: UserClient,
+    private val accessTokenGenerator: AccessTokenGenerator,
+    private val studentWriter: StudentWriter,
+) {
+    fun signIn(command: SignInCommand): SignInResponse {
+        val token: TokenResponse = authClient.signIn(
+            SignInClientRequest(
+                email = command.email,
+                password = command.password
+            )
+        )
+        return signInSsumgo(token)
+    }
+
+    fun issueRefreshToken(request: RefreshTokenRequest): SignInResponse {
+        val token = authClient.getRefreshToken(
+            RefreshTokenClientRequest(
+                request.refreshToken
+            )
+        )
+        return signInSsumgo(token)
+    }
+
+    private fun signInSsumgo(token: TokenResponse): SignInResponse {
+        val userResponse: UserResponse = userClient.getUser(accessTokenGenerator.formatingBearer(token.accessToken))
+        val student = studentWriter.signIn(userResponse.toDomain())
+        return SignInResponse(
+            accessToken = accessTokenGenerator.generateToken(student.id!!),
+            refreshToken = token.refreshToken,
+            studentId = student.id,
+        )
+    }
+}
