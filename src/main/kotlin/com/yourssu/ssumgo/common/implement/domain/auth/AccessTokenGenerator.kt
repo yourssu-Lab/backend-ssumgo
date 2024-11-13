207fbb24e1c143d8eb829efb5688623a2b99eba8
dwl21 <nggus5@gmail.com>
2024-11-13 22:52:33 +0900

    feat: implement an accessToken generator

---
+++ src/main/kotlin/com/yourssu/ssumgo/common/implement/domain/auth/AccessTokenGenerator.kt
@@ -0,0 +1,17 @@
+package com.yourssu.ssumgo.common.implement.domain.auth
+
+import com.yourssu.ssumgo.common.storage.domain.auth.JwtTokenProvider
+import org.springframework.stereotype.Component
+
+@Component
+class AccessTokenGenerator(
+    private val jwtTokenProvider: JwtTokenProvider
+) {
+    fun generateToken(id: Long): String {
+        return jwtTokenProvider.generateTokenWithId(id)
+    }
+
+    fun formatingBearer(token: String): String {
+        return "Bearer $token"
+    }
+}
