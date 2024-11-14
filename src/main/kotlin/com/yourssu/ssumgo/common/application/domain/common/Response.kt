300a291cfcbd292e0ed655e444d206a065f894f2
dwl21 <nggus5@gmail.com>
2024-11-14 14:05:56 +0900

    feat: add a timestamp in a response dto

--- src/main/kotlin/com/yourssu/ssumgo/common/application/domain/common/Response.kt
+++ src/main/kotlin/com/yourssu/ssumgo/common/application/domain/common/Response.kt
@@ -1,6 +1,10 @@
 package com.yourssu.ssumgo.common.application.domain.common
 
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 class Response<T>(
+    val timestamp: String = LocalDateTime.now().format(DateTimeFormatter.ofPattern("YYYY-MM-dd HH:mm:ss")),
     val result: T
 ) {
 }
