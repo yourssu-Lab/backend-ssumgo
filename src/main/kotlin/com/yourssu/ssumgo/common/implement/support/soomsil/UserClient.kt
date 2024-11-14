85cd227b65a729c17e6dc96cfacfece8e69af1d5
dwl21 <nggus5@gmail.com>
2024-11-14 14:06:31 +0900

    refactor: use a constant var

--- src/main/kotlin/com/yourssu/ssumgo/common/implement/support/soomsil/UserClient.kt
+++ src/main/kotlin/com/yourssu/ssumgo/common/implement/support/soomsil/UserClient.kt
@@ -2,6 +2,7 @@ package com.yourssu.ssumgo.common.implement.support.soomsil
 
 import com.yourssu.ssumgo.student.implement.domain.student.ProfileImageUrls
 import com.yourssu.ssumgo.student.implement.domain.student.Student
+import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestHeader
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestHeader
 @Component
 interface UserClient {
     @GetMapping
-    fun getUser(@RequestHeader("Authorization") token: String): UserResponse
+    fun getUser(@RequestHeader(value = HttpHeaders.AUTHORIZATION) token: String): UserResponse
 }
 
 data class UserResponse(
