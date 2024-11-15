d3ec773cabc1149949f746a761abb7346864126d
dwl21 <nggus5@gmail.com>
2024-11-15 21:49:24 +0900

    fix: change a responseBody in getStudent api

--- src/main/kotlin/com/yourssu/ssumgo/student/application/domain/student/StudentController.kt
+++ src/main/kotlin/com/yourssu/ssumgo/student/application/domain/student/StudentController.kt
@@ -1,8 +1,10 @@
 package com.yourssu.ssumgo.student.application.domain.student
 
+import com.yourssu.ssumgo.common.application.domain.common.Response
 import com.yourssu.ssumgo.common.implement.domain.auth.StudentId
 import com.yourssu.ssumgo.student.business.domain.student.StudentResponse
 import com.yourssu.ssumgo.student.business.domain.student.StudentService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -13,7 +15,7 @@ class StudentController(
     private val studentService: StudentService,
 ) {
     @GetMapping
-    fun getStudent(@StudentId id: Long): StudentResponse {
-        return studentService.getStudent(id)
+    fun getStudent(@StudentId id: Long): ResponseEntity<Response<StudentResponse>> {
+        return ResponseEntity.ok(Response(result = studentService.getStudent(id)))
     }
 }
