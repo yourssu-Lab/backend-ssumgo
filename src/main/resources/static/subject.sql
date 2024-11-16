04b375388f429d6ce461d78b290f73ba6f7c995b
dwl21 <nggus5@gmail.com>
2024-11-16 22:00:59 +0900

    docs: complete Posts APIs

---
+++ src/main/resources/static/subject.sql
@@ -0,0 +1,17 @@
+INSERT INTO subject (
+    subject_name,
+    professor_name,
+    completion,
+    subject_code,
+    department,
+    time,
+    credit
+) VALUES (
+             '데이터베이스',
+             '김교수',
+             '전공필수',
+             12345,
+             '컴퓨터학부',
+             3,
+             3
+         );
