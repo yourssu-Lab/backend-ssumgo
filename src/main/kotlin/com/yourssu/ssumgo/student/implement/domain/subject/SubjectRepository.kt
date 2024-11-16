4abd0138ef3836aca6ce2bd6f06368dba2fd6b05
dwl21 <nggus5@gmail.com>
2024-11-16 21:57:23 +0900

    feat: implement getSubject

--- src/main/kotlin/com/yourssu/ssumgo/student/implement/domain/subject/SubjectRepository.kt
+++ src/main/kotlin/com/yourssu/ssumgo/student/implement/domain/subject/SubjectRepository.kt
@@ -3,4 +3,5 @@ package com.yourssu.ssumgo.student.implement.domain.subject
 interface SubjectRepository {
     fun save(subject: Subject): Subject
     fun getAllSubjects(): List<Subject>
+    fun get(subjectId: Long): Subject
 }
