7c0127f6bbe0deb89abfdba0a4f4f2e2b32ca12b
dwl21 <nggus5@gmail.com>
2024-11-19 21:28:06 +0900

    fix: wrong access modifier in SubjectResponse

--- src/main/kotlin/com/yourssu/ssumgo/student/business/domain/subject/SubjectResponse.kt
+++ src/main/kotlin/com/yourssu/ssumgo/student/business/domain/subject/SubjectResponse.kt
@@ -3,14 +3,14 @@ package com.yourssu.ssumgo.student.business.domain.subject
 import com.yourssu.ssumgo.student.implement.domain.subject.Subject
 
 data class SubjectResponse(
-    private val subjectId: Long,
-    private val subjectName: String,
-    private val professorName: String,
-    private val completion: String,
-    private val subjectCode: Int,
-    private val department: String,
-    private val time: Int,
-    private val credit: Int,
+    val subjectId: Long,
+    val subjectName: String,
+    val professorName: String,
+    val completion: String,
+    val subjectCode: Int,
+    val department: String,
+    val time: Int,
+    val credit: Int,
 ){
     companion object {
         fun from(subject: Subject): SubjectResponse {
