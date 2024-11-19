9ecd1e7034d92d6b27e43051676706a1eba717a7
dwl21 <nggus5@gmail.com>
2024-11-19 18:02:22 +0900

    feat: change a status code of a createSubject API to 201

--- src/main/kotlin/com/yourssu/ssumgo/student/application/domain/subject/SubjectController.kt
+++ src/main/kotlin/com/yourssu/ssumgo/student/application/domain/subject/SubjectController.kt
@@ -13,6 +13,7 @@ import jakarta.validation.Valid
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Positive
 import org.hibernate.validator.constraints.Range
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
@@ -26,7 +27,8 @@ class SubjectController(
 ) {
     @PostMapping
     fun createSubject(@Valid @RequestBody request: SubjectCreatedRequest): ResponseEntity<Response<SubjectResponse>> {
-        return ResponseEntity.ok(Response(result = subjectService.saveSubject(request.toCommand())))
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(Response(result = subjectService.saveSubject(request.toCommand())))
     }
 
     @GetMapping
