7e026ce16ddc356edeaea4819ddba64017b54505
dwl21 <nggus5@gmail.com>
2024-11-19 16:45:56 +0900

    fix: valid one a post to a comment

--- src/main/kotlin/com/yourssu/ssumgo/student/implement/domain/comment/CommentWriter.kt
+++ src/main/kotlin/com/yourssu/ssumgo/student/implement/domain/comment/CommentWriter.kt
@@ -5,10 +5,15 @@ import org.springframework.transaction.annotation.Transactional
 
 @Component
 class CommentWriter(
-    private val commentRepository: CommentRepository
+    private val commentRepository: CommentRepository,
 ) {
     @Transactional
     fun save(comment: Comment): Comment {
+        if (commentRepository.existsComment(comment.posts)) {
+            throw CommentExistException()
+        }
         return commentRepository.save(comment)
     }
 }
+
+class CommentExistException() : RuntimeException(message = "해당 질문에 이미 답변이 존재합니다.")
