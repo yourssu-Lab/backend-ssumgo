b4733f10f8b26de50643d4b81e9cc52da45e96a1
dwl21 <nggus5@gmail.com>
2024-11-19 17:52:10 +0900

    feat: implement created subjects

--- src/main/kotlin/com/yourssu/ssumgo/student/business/domain/subject/SubjectService.kt
+++ src/main/kotlin/com/yourssu/ssumgo/student/business/domain/subject/SubjectService.kt
@@ -1,12 +1,19 @@
 package com.yourssu.ssumgo.student.business.domain.subject
 
+import com.yourssu.ssumgo.student.implement.domain.subject.Subject
 import com.yourssu.ssumgo.student.implement.domain.subject.SubjectReader
+import com.yourssu.ssumgo.student.implement.domain.subject.SubjectWriter
 import org.springframework.stereotype.Service
 
 @Service
 class SubjectService(
-    private val subjectReader: SubjectReader
+    private val subjectReader: SubjectReader,
+    private val subjectWriter: SubjectWriter,
 ) {
+    fun saveSubject(command: SubjectCreatedCommand): SubjectResponse {
+        return SubjectResponse.from(subjectWriter.save(command.toDomain()))
+    }
+
     fun getSubjectsByStudent(studentId: Long, years: Int, semester: Int): List<SubjectResponse> {
         return subjectReader.getSubjects(studentId, years, semester).map { SubjectResponse.from(it) }
     }
@@ -14,4 +21,26 @@ class SubjectService(
     fun getAllSubjects(): List<SubjectResponse> {
         return subjectReader.getAllSubjects().map { SubjectResponse.from(it) }
     }
+}
+
+data class SubjectCreatedCommand(
+    val subjectName: String,
+    val professorName: String,
+    val completion: String,
+    val subjectCode: Int,
+    val department: String,
+    var time: Int,
+    var credit: Int,
+) {
+   fun toDomain(): Subject {
+       return Subject(
+           subjectName = subjectName,
+           professorName = professorName,
+           completion = completion,
+           subjectCode = subjectCode,
+           department = department,
+           time = time,
+           credit = credit,
+       )
+   }
 }
--- src/main/kotlin/com/yourssu/ssumgo/student/storage/domain/subject/SubjectRepositoryImpl.kt
+++ src/main/kotlin/com/yourssu/ssumgo/student/storage/domain/subject/SubjectRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.yourssu.ssumgo.student.storage.domain.subject
 
+import com.yourssu.ssumgo.common.application.domain.common.ForbiddenException
 import com.yourssu.ssumgo.common.application.domain.common.NotFoundException
 import com.yourssu.ssumgo.student.implement.domain.subject.Subject
 import com.yourssu.ssumgo.student.implement.domain.subject.SubjectRepository
@@ -12,6 +13,9 @@ class SubjectRepositoryImpl(
     private val subjectJpaRepository: SubjectJpaRepository
 ) : SubjectRepository {
     override fun save(subject: Subject): Subject {
+        if (subjectJpaRepository.existsBySubjectCode(subject.subjectCode)) {
+            throw SubjectAlreadyExistsException()
+        }
         return subjectJpaRepository.save(SubjectEntity.toEntity(subject)).toDomain()
     }
 
@@ -28,6 +32,11 @@ class SubjectRepositoryImpl(
 interface SubjectJpaRepository : JpaRepository<SubjectEntity, Long> {
     @Query("select s from SubjectEntity s where s.id = :id")
     fun get(id: Long): SubjectEntity?
+    @Query("select (count(s) > 0) from SubjectEntity s where s.subjectCode = :subjectCode")
+    fun existsBySubjectCode(subjectCode: Int): Boolean
+
 }
 
 class SubjectNotFoundException : NotFoundException(message = "해당하는 과목이 없습니다.")
+
+class SubjectAlreadyExistsException : ForbiddenException(message = "이미 존재하는 과목코드입니다.")
