5fa597ce2e1dc65ca50bc230ae71402c2a81aaa1
dwl21 <nggus5@gmail.com>
2024-11-20 19:28:56 +0900

    test: write dci tests in a commentRepo

---
+++ src/test/kotlin/com/yourssu/ssumgo/common/support/fixture/CommentFixture.kt
@@ -0,0 +1,24 @@
+package com.yourssu.ssumgo.common.support.fixture
+
+import com.yourssu.ssumgo.student.implement.domain.comment.Comment
+import com.yourssu.ssumgo.student.implement.domain.posts.Posts
+import com.yourssu.ssumgo.student.implement.domain.student.Student
+
+enum class CommentFixture(
+    val title: String,
+    val content: String,
+) {
+    COMMENT(
+        title = "title_comment",
+        content = "content_comment"
+    );
+
+    fun toDomain(mentor: Student, posts: Posts): Comment {
+        return Comment(
+            mentor = mentor,
+            posts = posts,
+            title = title,
+            content = content,
+        )
+    }
+}
