901b622d5d21e26f2b460f86ba6b03f22ab8246d
dwl21 <nggus5@gmail.com>
2024-11-20 18:14:37 +0900

    feat: implement existsSubjectStudent

--- src/main/kotlin/com/yourssu/ssumgo/student/implement/domain/subject/StudentSubjectRepository.kt
+++ src/main/kotlin/com/yourssu/ssumgo/student/implement/domain/subject/StudentSubjectRepository.kt
@@ -4,6 +4,7 @@ import com.yourssu.ssumgo.student.implement.domain.student.Student
 
 
 interface StudentSubjectRepository {
+    fun save(student: Student, subject: Subject, years: Int, semester: Int): Subject
+    fun existsStudentSubject(studentId: Long, subjectId: Long, years: Int, semester: Int): Boolean
     fun getSubjects(studentId: Long, years: Int, semester: Int): List<Subject>
-    fun save(student: Student, subject: Subject, years: Int, semester: Int): Long
 }
