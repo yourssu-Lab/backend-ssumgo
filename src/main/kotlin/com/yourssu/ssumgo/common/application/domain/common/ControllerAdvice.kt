e260be02fbfb3d025cdc0184b40d3b2b98c91802
dwl21 <nggus5@gmail.com>
2024-11-20 18:15:56 +0900

    fix: declare 409 Conflict

--- src/main/kotlin/com/yourssu/ssumgo/common/application/domain/common/ControllerAdvice.kt
+++ src/main/kotlin/com/yourssu/ssumgo/common/application/domain/common/ControllerAdvice.kt
@@ -20,6 +20,7 @@ class ControllerAdvice {
         private const val VALIDATION_DEFAULT_ERROR_MESSAGE = "Unknown validation error"
         private const val INVALID_REQUEST_DELIMITER = ", "
     }
+
     @ExceptionHandler(Exception::class)
     fun handleException(e: Exception): ResponseEntity<ErrorResponse> {
         logger.error(InternalServerError().message, e)
@@ -37,21 +38,28 @@ class ControllerAdvice {
     @ExceptionHandler(NotFoundException::class)
     fun handleNotFound(e: NotFoundException): ResponseEntity<ErrorResponse> {
         logger.error(e.message, e)
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+        return ResponseEntity.status(e.status)
             .body(ErrorResponse.from(e))
     }
 
     @ExceptionHandler(UnauthorizedException::class)
     fun handleUnauthorized(e: UnauthorizedException): ResponseEntity<ErrorResponse> {
         logger.error(e.message, e)
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+        return ResponseEntity.status(e.status)
             .body(ErrorResponse.from(e))
     }
 
     @ExceptionHandler(ForbiddenException::class)
     fun handleForbidden(e: ForbiddenException): ResponseEntity<ErrorResponse> {
         logger.error(e.message, e)
-        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+        return ResponseEntity.status(e.status)
+            .body(ErrorResponse.from(e))
+    }
+
+    @ExceptionHandler(ConflictException::class)
+    fun handleConflict(e: ConflictException): ResponseEntity<ErrorResponse> {
+        logger.error(e.message, e)
+        return ResponseEntity.status(e.status)
             .body(ErrorResponse.from(e))
     }
 
@@ -64,10 +72,12 @@ class ControllerAdvice {
             .joinToString(INVALID_REQUEST_DELIMITER) { "Invalid Input: [$it]" }
         logger.error(errorMessage, bindingResult)
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-            .body(ErrorResponse(
-                status = HttpStatus.BAD_REQUEST.value(),
-                message = errorMessage
-            ))
+            .body(
+                ErrorResponse(
+                    status = HttpStatus.BAD_REQUEST.value(),
+                    message = errorMessage
+                )
+            )
     }
 
     @ExceptionHandler(ConstraintViolationException::class)
@@ -76,10 +86,12 @@ class ControllerAdvice {
     ): ResponseEntity<ErrorResponse> {
         logger.error(e.message, e)
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-            .body(ErrorResponse(
-                status = HttpStatus.BAD_REQUEST.value(),
-                message = e.message ?: VALIDATION_DEFAULT_ERROR_MESSAGE
-            ))
+            .body(
+                ErrorResponse(
+                    status = HttpStatus.BAD_REQUEST.value(),
+                    message = e.message ?: VALIDATION_DEFAULT_ERROR_MESSAGE
+                )
+            )
     }
 
     @ExceptionHandler(FeignClientException::class)
@@ -88,10 +100,12 @@ class ControllerAdvice {
     ): ResponseEntity<ErrorResponse> {
         logger.error(e.message, e)
         return ResponseEntity.status(e.status())
-            .body(ErrorResponse(
-                status = e.status(),
-                message = "숨쉴때 API 호출 중 에러가 발생했습니다. {{ ${e.message} }}"
-            ))
+            .body(
+                ErrorResponse(
+                    status = e.status(),
+                    message = "숨쉴때 API 호출 중 에러가 발생했습니다. {{ ${e.message} }}"
+                )
+            )
     }
 
     @ExceptionHandler(NoResourceFoundException::class)
@@ -100,10 +114,12 @@ class ControllerAdvice {
     ): ResponseEntity<ErrorResponse> {
         logger.error(e.message, e)
         return ResponseEntity.status(e.statusCode)
-            .body(ErrorResponse(
-                status = e.statusCode.value(),
-                message = "올바르지 않은 경로입니다. {{ {baseUrl}/${e.resourcePath} }}"
-            ))
+            .body(
+                ErrorResponse(
+                    status = e.statusCode.value(),
+                    message = "올바르지 않은 경로입니다. {{ /${e.resourcePath} }}"
+                )
+            )
     }
 
     @ExceptionHandler(DuplicateKeyException::class)
@@ -112,10 +128,12 @@ class ControllerAdvice {
     ): ResponseEntity<ErrorResponse> {
         logger.error(e.message, e)
         return ResponseEntity.status(HttpStatus.CONFLICT)
-            .body(ErrorResponse(
-                status = HttpStatus.CONFLICT.value(),
-                message = "중복된 아이디가 존재합니다. {{ ${e.message} }}"
-            ))
+            .body(
+                ErrorResponse(
+                    status = HttpStatus.CONFLICT.value(),
+                    message = "중복된 아이디가 존재합니다. {{ ${e.message} }}"
+                )
+            )
     }
 }
 
@@ -132,6 +150,7 @@ data class ErrorResponse(
             )
         }
     }
+
 }
 
 abstract class Error(
@@ -166,5 +185,11 @@ open class UnauthorizedException(
 open class ForbiddenException(
     status: HttpStatus = HttpStatus.FORBIDDEN,
     message: String = "권한이 없습니다.",
+) : Error(status, message) {
+}
+
+open class ConflictException(
+    status: HttpStatus = HttpStatus.CONFLICT,
+    message: String = "이미 존재하는 리소스입니다.",
 ) : Error(status, message) {
 }
