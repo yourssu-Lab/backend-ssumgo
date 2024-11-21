bfba47af5a1ca943eab594d3a4a6a6b5b5f6a875
dwl21 <nggus5@gmail.com>
2024-11-21 16:49:06 +0900

    test: write dci tests in a authService

--- src/test/kotlin/com/yourssu/ssumgo/common/business/domain/auth/AuthServiceTest.kt
+++ src/test/kotlin/com/yourssu/ssumgo/common/business/domain/auth/AuthServiceTest.kt
@@ -6,17 +6,15 @@ import com.yourssu.ssumgo.common.implement.support.soomsil.TokenResponse
 import com.yourssu.ssumgo.common.implement.support.soomsil.UserResponse
 import com.yourssu.ssumgo.common.storage.support.soomsil.AuthFeignClientImpl
 import com.yourssu.ssumgo.common.storage.support.soomsil.SoomsilFeignClientImpl
-import org.junit.jupiter.api.Test
+import com.yourssu.ssumgo.common.support.config.ApplicationTest
+import org.junit.jupiter.api.*
 import org.mockito.Mockito.`when`
 import org.mockito.kotlin.any
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.bean.override.mockito.MockitoBean
-import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
-@SpringBootTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@ApplicationTest
 class AuthServiceTest {
     @Autowired
     private lateinit var authService: AuthService
@@ -27,47 +25,56 @@ class AuthServiceTest {
     @MockitoBean
     private lateinit var userClient: SoomsilFeignClientImpl
 
-    @Test
-    fun signIn() {
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores::class)
+    inner class signIn_메서드는 {
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores::class)
+        inner class 가입되어_있는_유어슈_유저이면 {
+            @Test
+            @DisplayName("엑세스_토큰과_리프레시_토큰을_반환한다.")
+            fun success() {
+                val response = signIn()
+
+                assertNotNull(response)
+            }
+        }
+    }
+
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores::class)
+    inner class issueRefreshToken_메서드는 {
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores::class)
+        inner class 올바른_리프레시_토큰이면 {
+            @Test
+            @DisplayName("엑세스_토큰과_리프레시_토큰을_반환한다.")
+            fun success() {
+                `when`(authClient.getRefreshToken(any()))
+                    .thenReturn(TokenResponse("access.Token.sign", "refresh.Token.sign"))
+                val response = authService.issueRefreshToken(RefreshTokenRequest(signIn().refreshToken))
+
+                assertNotNull(response)
+            }
+        }
+    }
+
+    private fun signIn(email: String = "leo@soongsil.ac.kr"): SignInResponse {
         `when`(authClient.signIn(any()))
             .thenReturn(TokenResponse("access.Token.sign", "refresh.Token.sign"))
         `when`(userClient.getUser(any()))
             .thenReturn(
                 UserResponse(
-                    email = "leo@soongsil.ac.kr",
+                    email = email,
                     nickName = "nickname",
                     profileImage = ProfileImageUrlsResponse(
                         smallUrl = "smallUrl",
                         midUrl = "profileUrl",
                         largeUrl = "largeUrl"
                     )
-                ))
-        val signInCommand = SignInCommand("leo@soongsil.ac.kr", "password")
-
-        val response: SignInResponse = authService.signIn(signInCommand)
-
-        assertEquals(1L, response.studentId)
-    }
-
-    @Test
-    fun issueRefreshToken() {
-        `when`(authClient.getRefreshToken(any()))
-            .thenReturn(TokenResponse("access.Token.refresh", "refresh.Token.refresh"))
-        `when`(userClient.getUser(any()))
-            .thenReturn(
-                UserResponse(
-                    email = "leo@soongsil.ac.kr",
-                    nickName = "nickname",
-                    profileImage = ProfileImageUrlsResponse(
-                        smallUrl = "smallUrl",
-                        midUrl = "profileUrl",
-                        largeUrl = "largeUrl"
-                    )
-                ))
-        val refreshTokenCommand = RefreshTokenRequest("refresh.Token.refresh")
-
-        val response = authService.issueRefreshToken(refreshTokenCommand)
-
-        assertEquals(1L, response.studentId)
+                )
+            )
+        val signInCommand = SignInCommand(email, "password")
+        return authService.signIn(signInCommand)
     }
 }
