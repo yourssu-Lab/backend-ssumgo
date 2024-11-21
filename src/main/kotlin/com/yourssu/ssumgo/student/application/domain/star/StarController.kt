1cfbaba0b3b8436b694c93e65cbfb97d9d3b66b9
dwl21 <nggus5@gmail.com>
2024-11-21 20:52:09 +0900

    feat: complete Star APIs

---
+++ src/main/kotlin/com/yourssu/ssumgo/student/application/domain/star/StarController.kt
@@ -0,0 +1,50 @@
+package com.yourssu.ssumgo.student.application.domain.star
+
+import com.yourssu.ssumgo.common.application.domain.common.Response
+import com.yourssu.ssumgo.common.implement.domain.auth.StudentId
+import com.yourssu.ssumgo.student.business.domain.comment.CommentResponse
+import com.yourssu.ssumgo.student.business.domain.star.StarCreatedCommand
+import com.yourssu.ssumgo.student.business.domain.star.StarService
+import com.yourssu.ssumgo.student.implement.domain.posts.SortBy
+import com.yourssu.ssumgo.student.storage.domain.comment.CommentsPage
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Positive
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/drawers")
+@Validated
+class StarController(
+    private val starService: StarService
+) {
+    @PostMapping
+    fun save(@StudentId studentId: Long, @Positive @RequestParam commentId: Long): ResponseEntity<Response<CommentResponse>> {
+        val response = starService.saveStar(StarCreatedCommand(commentId = commentId, studentId = studentId))
+        return ResponseEntity.status(HttpStatus.CREATED).body(Response(result = response))
+    }
+
+    @GetMapping("/students")
+    fun getAllByStudent(
+        @StudentId studentId: Long,
+        @Positive @RequestParam(defaultValue = "1") page: Int,
+        @NotBlank @RequestParam(defaultValue = "latest") sortBy: String,
+        @Positive @RequestParam(defaultValue = "10") size: Int,
+    ): ResponseEntity<Response<CommentsPage>> {
+        val response = starService.getAllByStudent(
+            studentId = studentId,
+            pageNumber = page,
+            pageSize = size,
+            sortBy = SortBy.of(sortBy)
+        )
+        return ResponseEntity.ok(Response(result = response))
+    }
+
+    @DeleteMapping("/comments/{commentId}")
+    fun deleteByComment(@StudentId studentId: Long, @Positive @PathVariable commentId: Long): ResponseEntity<Void> {
+        starService.deleteByComment(studentId = studentId, commentId = commentId)
+        return ResponseEntity.noContent().build()
+    }
+}
---
+++ src/test/kotlin/com/yourssu/ssumgo/student/business/domain/star/StarServiceTest.kt
@@ -0,0 +1,99 @@
+package com.yourssu.ssumgo.student.business.domain.star
+
+import com.yourssu.ssumgo.common.support.config.ApplicationTest
+import com.yourssu.ssumgo.common.support.fixture.CommentFixture.COMMENT
+import com.yourssu.ssumgo.common.support.fixture.PageFixture.PAGE_LATEST
+import com.yourssu.ssumgo.common.support.fixture.PostFixture
+import com.yourssu.ssumgo.common.support.fixture.StudentFixture
+import com.yourssu.ssumgo.common.support.fixture.SubjectFixture
+import com.yourssu.ssumgo.student.implement.domain.comment.CommentWriter
+import com.yourssu.ssumgo.student.implement.domain.posts.PostsWriter
+import com.yourssu.ssumgo.student.implement.domain.star.Star
+import com.yourssu.ssumgo.student.implement.domain.star.StarWriter
+import com.yourssu.ssumgo.student.implement.domain.student.Student
+import com.yourssu.ssumgo.student.implement.domain.student.StudentWriter
+import com.yourssu.ssumgo.student.implement.domain.subject.SubjectWriter
+import org.junit.jupiter.api.*
+import org.springframework.beans.factory.annotation.Autowired
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
+@ApplicationTest
+class StarServiceTest {
+    @Autowired
+    private lateinit var starService: StarService
+
+    @Autowired
+    private lateinit var studentWriter: StudentWriter
+
+    @Autowired
+    private lateinit var subjectWriter: SubjectWriter
+
+    @Autowired
+    private lateinit var postsWriter: PostsWriter
+
+    @Autowired
+    private lateinit var commentWriter: CommentWriter
+
+    @Autowired
+    private lateinit var starWriter: StarWriter
+
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores::class)
+    inner class saveStar_메서드는 {
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores::class)
+        inner class 등록된_학생과_답변_이면 {
+            @Test
+            @DisplayName("보관함에 등록한다.")
+            fun success() {
+                val mentee = studentWriter.save(StudentFixture.STUDENT_LEO.toDomain())
+                val mentor = studentWriter.save(StudentFixture.STUDENT_MAI.toDomain())
+                val subject = subjectWriter.save(SubjectFixture.SUBJECT_1.toDomain())
+                val posts = postsWriter.save(PostFixture.POST.toDomain(mentee, subject))
+                val comment = commentWriter.save(COMMENT.toDomain(mentor, posts))
+                val command = StarCreatedCommand(
+                    commentId = comment.id!!,
+                    studentId = mentee.id!!,
+                )
+
+                val response = starService.saveStar(command)
+
+                assertNotNull(response)
+            }
+        }
+    }
+
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores::class)
+    inner class getAllByStudent_메서드는 {
+        var student: Student? = null
+
+        @BeforeEach
+        fun setUp() {
+            student = studentWriter.save(StudentFixture.STUDENT_MAI.toDomain())
+            val mentee = studentWriter.save(StudentFixture.STUDENT_LEO.toDomain())
+            val subject = subjectWriter.save(SubjectFixture.SUBJECT_1.toDomain())
+            val posts = postsWriter.save(PostFixture.POST.toDomain(mentee, subject))
+            val comment = commentWriter.save(COMMENT.toDomain(mentee, posts))
+            starWriter.save(Star(comment = comment, student = student!!))
+        }
+
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores::class)
+        inner class 학생이_등록한_좋아요가_존재하면 {
+            @Test
+            @DisplayName("보관함에 있는 답변을 모두 반환한다.")
+            fun success() {
+                val commentsPage = starService.getAllByStudent(
+                    student!!.id!!,
+                    PAGE_LATEST.page,
+                    PAGE_LATEST.size,
+                    PAGE_LATEST.sortBy
+                )
+
+                assertEquals(1, commentsPage.totalElements)
+            }
+        }
+    }
+}
