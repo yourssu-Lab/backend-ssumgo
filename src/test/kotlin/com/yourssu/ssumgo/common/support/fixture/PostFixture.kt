c30b442600bea1cca79191797b2341bdf474232e
dwl21 <nggus5@gmail.com>
2024-11-21 17:20:33 +0900

    test: write dci tests in a postService

--- src/test/kotlin/com/yourssu/ssumgo/common/support/fixture/PostFixture.kt
+++ src/test/kotlin/com/yourssu/ssumgo/common/support/fixture/PostFixture.kt
@@ -1,5 +1,6 @@
 package com.yourssu.ssumgo.common.support.fixture
 
+import com.yourssu.ssumgo.student.business.domain.posts.PostsCreatedCommand
 import com.yourssu.ssumgo.student.implement.domain.posts.Posts
 import com.yourssu.ssumgo.student.implement.domain.student.Student
 import com.yourssu.ssumgo.student.implement.domain.subject.Subject
@@ -17,6 +18,15 @@ enum class PostFixture(
         return Posts(
             mentee = mentee,
             subject = subject,
+            title = title,
+            content = content,
+        )
+    }
+
+    fun toPostsCreatedCommand(menteeId: Long, subjectId: Long): PostsCreatedCommand {
+        return PostsCreatedCommand(
+            menteeId = menteeId,
+            subjectId = subjectId,
             title = title,
             content = content,
         )
