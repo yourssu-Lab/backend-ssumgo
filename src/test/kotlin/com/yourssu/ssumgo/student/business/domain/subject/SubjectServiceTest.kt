7abdea7a9155ee79a10a40ac601746d2b26cf704
dwl21 <nggus5@gmail.com>
2024-11-21 16:36:54 +0900

    test: write dci tests in a subjectService

--- src/test/kotlin/com/yourssu/ssumgo/student/business/domain/subject/SubjectServiceTest.kt
+++ src/test/kotlin/com/yourssu/ssumgo/student/business/domain/subject/SubjectServiceTest.kt
@@ -1,20 +1,18 @@
 package com.yourssu.ssumgo.student.business.domain.subject
 
-import com.yourssu.ssumgo.student.implement.domain.student.ProfileImageUrls
+import com.yourssu.ssumgo.common.support.config.ApplicationTest
+import com.yourssu.ssumgo.common.support.fixture.StudentFixture
+import com.yourssu.ssumgo.common.support.fixture.SubjectFixture.SUBJECT_1
 import com.yourssu.ssumgo.student.implement.domain.student.Student
 import com.yourssu.ssumgo.student.implement.domain.student.StudentWriter
-import com.yourssu.ssumgo.student.implement.domain.subject.Subject
 import com.yourssu.ssumgo.student.implement.domain.subject.SubjectWriter
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.*
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.annotation.DirtiesContext
-import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
-@SpringBootTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@ApplicationTest
 class SubjectServiceTest {
-
     @Autowired
     private lateinit var subjectService: SubjectService
 
@@ -24,72 +22,65 @@ class SubjectServiceTest {
     @Autowired
     private lateinit var studentWriter: StudentWriter
 
-    @Test
-    fun save() {
-        val command = SubjectCreatedCommand(
-            subjectName = "과목명",
-            professorName = "교수명",
-            completion = "이수구분",
-            subjectCode = 1234,
-            time = 1,
-            department = "학과",
-            credit = 3
-        )
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores::class)
+    inner class saveSubject_메서드는 {
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores::class)
+        inner class 과목_생성_요청_커멘드가_주어지면 {
+            @Test
+            @DisplayName("과목을_생성한다.")
+            fun success() {
+                val response = subjectService.saveSubject(SUBJECT_1.toCreatedCommand())
 
-        val response = subjectService.saveSubject(command)
-
-        assertNotNull(response)
+                assertNotNull(response)
+            }
+        }
     }
 
-    @Test
-    fun getAllSubjects() {
-        val subject = Subject(
-            subjectName = "과목명",
-            professorName = "교수명",
-            completion = "이수구분",
-            subjectCode = 1234,
-            time = 1,
-            department = "학과",
-            credit = 3
-        )
-        subjectWriter.save(subject)
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores::class)
+    inner class getAllSubjects_메서드는 {
+        @BeforeEach
+        fun setUp() {
+            subjectService.saveSubject(SUBJECT_1.toCreatedCommand())
+        }
 
-        val subjects = subjectService.getAllSubjects()
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores::class)
+        inner class 과목이_존재하면 {
+            @Test
+            @DisplayName("존재하는_모든 과목을 반환한다.")
+            fun success() {
+                val response = subjectService.getAllSubjects()
 
-        assertEquals(1, subjects.size)
+                assertEquals(1, response.size)
+            }
+        }
     }
 
-    @Test
-    fun getSubjectsByStudent() {
-        //given
-        val student = Student(
-            yourssuId = "yourssuId",
-            nickname = "nickname",
-            profileImageUrls = ProfileImageUrls(
-                smallUrl = "smallUrl",
-                midUrl = "mediumUrl",
-                largeUrl = "largeUrl",
-            )
-        )
-        val savedStudent = studentWriter.signIn(student)
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores::class)
+    inner class getSubjectsByStudent_메서드는 {
+        private var student: Student? = null
 
-        val subject = Subject(
-            subjectName = "과목명",
-            professorName = "교수명",
-            completion = "이수구분",
-            subjectCode = 1234,
-            time = 1,
-            department = "학과",
-            credit = 3
-        )
-        val savedSubject = subjectWriter.save(subject)
+        @BeforeEach
+        fun setUp() {
+            student = studentWriter.signIn(StudentFixture.STUDENT_LEO.toStudent())
+            val subject = subjectWriter.save(SUBJECT_1.toDomain())
+            subjectWriter.saveStudentSubject(student!!, subject, 2024, 2)
+        }
 
-        subjectWriter.saveStudentSubject(savedStudent, savedSubject, 2024, 2)
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores::class)
+        inner class 학생_정보가_주어지면 {
+            @Test
+            @DisplayName("학생이 수강하는 모든 과목을 반환한다.")
+            fun success() {
+                val response = subjectService.getSubjectsByStudent(student!!.id!!, 2024, 2)
 
-        //when
-        val subjects = subjectService.getSubjectsByStudent(savedStudent.id!!, 2024, 2)
-
-        //then
-        assertEquals(1, subjects.size)
+                assertEquals(1, response.size)
+            }
+        }
     }
 }
